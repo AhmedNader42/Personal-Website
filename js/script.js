ca421db24b53744f2a0d2b03e3383eb4d2533176
Ahmed Nader <ahmednader111@gmail.com>
2021-06-14 13:01:40 +0200

    Added the new demos. Added the gallery for screenshots

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 var slideIndex = 1;
+
+const tabs = ['breakpoint_demo_container', 'watch_demo_container', 'tinder_clone_demo_container', 'algo_club_demo_container'];
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -11,8 +13,8 @@ function currentSlide(n) {
 
 function showSlides(n) {
     var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
+    var slides = document.getElementsByClassName('mySlides');
+    var dots = document.getElementsByClassName('dot');
     if (n > slides.length) {
         slideIndex = 1;
     }
@@ -20,11 +22,17 @@ function showSlides(n) {
         slideIndex = slides.length;
     }
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i].style.display = 'none';
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+        dots[i].className = dots[i].className.replace(' active', '');
     }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].className += ' active';
+
+    for (let index = 0; index < tabs.length; index++) {
+        document.getElementById(tabs[index]).style.display = 'none';
+    }
+    const currentTab = document.getElementById(tabs[(slideIndex - 1) % tabs.length]);
+    currentTab.style.display = 'block';
 }
